2d52198453c2644b0eea2b406d0345f0d08551df
KawaiiMoe3 <tanjunsam3@gmail.com>
2024-08-15 00:23:03 +0800

    optimize ui

--- src/components/Projects.jsx
+++ src/components/Projects.jsx
@@ -13,11 +13,13 @@ export default function Projects() {
                 <div className='w-full max-w-xl lg:w-3/4'>
                     <h6 className='mb-2 font-semibold'>{project.title}</h6>
                     <p className='mb-4'>{project.description}</p>
-                    {project.technologies.map((tech, index) =>(
-                        <span key={index} className='mr-2 mt-4 rounded bg-purple-400 px-2 py-1 text-sm font-medium text-white'>
-                            {tech}
-                        </span>
-                    ))}
+                    <div className='flex flex-wrap'>
+                      {project.technologies.map((tech, index) =>(
+                          <span key={index} className='mr-2 mb-2 rounded bg-purple-400 px-2 py-1 text-sm font-medium text-white'>
+                              {tech}
+                          </span>
+                      ))}
+                    </div>
                 </div>
             </div>
         ))}
